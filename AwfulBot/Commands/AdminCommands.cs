8fe3348f33e444eec22d971563d0609585f1e8cd
SupSuper <supsuper@gmail.com>
2017-11-29 04:52:46 +0000

    Add admin commands

---
+++ AwfulBot/Commands/AdminCommands.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using DSharpPlus;
+using DSharpPlus.CommandsNext;
+using DSharpPlus.CommandsNext.Attributes;
+
+namespace AwfulBot.Commands
+{
+	[Group("admin")]
+	[Description("Administrative commands.")]
+	[Hidden]
+	[RequirePermissions(Permissions.ManageGuild)]
+	public class AdminCommands
+	{
+		[Command("restart")]
+		[Description("Reconnect to Discord")]
+		public async Task Restart(CommandContext ctx)
+		{
+			await ctx.Client.ReconnectAsync();
+		}
+
+		[Command("quit")]
+		[Description("Disconnect and exit bot")]
+		[Aliases("kill")]
+		public async Task Quit(CommandContext ctx)
+		{
+			await ctx.Client.DisconnectAsync();
+			Environment.Exit(0);
+		}
+	}
+}
